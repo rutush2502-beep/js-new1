8148599ce794050a3962bd6a06623571a053ad33
rutush2502-beep <rutush2502@gmail.com>
2025-08-08 05:18:50 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,5 +16,22 @@ let booleanIsLoogedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// %%%%%%%%%% OPERATIONS %%%%%%%%%%%
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "Hello"
+let str2 = " Ayush"
+ 
+let str3 = str1 + str2
+// console.log("Greeting to " + str3);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
