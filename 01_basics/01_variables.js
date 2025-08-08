abfcb2344de42a8d4b8d537c89df88beaf9530d2
rutush2502-beep <rutush2502@gmail.com>
2025-08-08 05:03:14 +0000

    conv of dt

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const acID = 123456;
+let email = "xyz@gamil.com";
+var pass = "1236589";
+city = "banglore";
+let accountState;
+
+//acID = 2
+email = "abc";
+pass = "9856";
+city = "delhi";
+
+console.log(acID);
+console.table([acID, email, pass, city, accountState]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "Ayush"
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
